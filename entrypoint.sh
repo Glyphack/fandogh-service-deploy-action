62d314d61940190a109293d3b885457be3be6bca
glyphack <sh.hooshyari@gmail.com>
2020-09-12 23:48:59 +0430

    create script to publish image and deploy service

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+username=$1
+password=$2
+image_name=$3
+image_version=$4
+path_to_service_file=$5
+
+COLLECT_ERROR=True fandogh login --username $username --password $password
+
+COLLECT_ERROR=True fandogh image init --name  $image_name
+COLLECT_ERROR=True fandogh image publish --version $image_version
+
+sed -i "s/VERSION>/$image_version/g" $path_to_service_file
+sed -i "s/<IMAGE/$image_name/g" $path_to_service_file
+
+COLLECT_ERROR=True fandogh service apply -f $path_to_service_file
